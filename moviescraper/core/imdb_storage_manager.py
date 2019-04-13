1147d11bdc91a67d5d5d654b62a069644fce1422
MANUEL ENRIQUE GOMEZ MONTERO <manuel.gomezmontero@telefonica.com>
2019-04-13 15:28:24 +0200

    Extract csv dataset. Add example dataset 100 movies.

--- moviescraper/core/imdb_storage_manager.py
+++ moviescraper/core/imdb_storage_manager.py
@@ -11,6 +11,23 @@ class IMDBStorageManager():
     def __init__(self, dbfile="db/imdb_dataset"):
         self._dbfile = dbfile
         self._db = UnQLite(self._dbfile)
+        self._movie_keys = ["id","@type","year", "title", "genre", "contentRating", 
+                            "description", "datePublished", "keywords", "duration",
+                            "rtng_all_v", "rtng_all_r","rtng_<18_v", "rtng_<18_r",
+                            "rtng_18-29_v", "rtng_18-29_r", "rtng_30-44_v",
+                            "rtng_30-44_r", "rtng_45+_v", "rtng_45+_r",
+                            "rtng_males_v", "rtng_males_r","rtng_m<18_v", "rtng_m<18_r",
+                            "rtng_m18-29_v", "rtng_m18-29_r", "rtng_m30-44_v",
+                            "rtng_m30-44_r", "rtng_m45+_v", "rtng_m45+_r",
+                            "rtng_females_v", "rtng_females_r","rtng_f<18_v", "rtng_f<18_r",
+                            "rtng_f18-29_v", "rtng_f18-29_r", "rtng_f30-44_v",
+                            "rtng_f30-44_r", "rtng_f45+_v", "rtng_f45+_r"]
+        self._user_keys = ["id", "name"]
+        self._person_keys = ["id", "name", "birth_date"]
+        self._person_movie_key = ["id_person", "id_movie", "rol"]
+        self._user_movie_key = ["id_user", "id_movie","global", "rate"]
+        self._rating_groups = ["all", ]
+        self._write_csv_headers()
 
     def write(self, doc, collection="movies"):
         if doc is not None:
@@ -20,36 +37,86 @@ class IMDBStorageManager():
             self._db.commit()
         if CSV:
             switcher = {
-                "users": "write_csv_user" ,
-                "movies": "write_csv_movie",
-                "persons": "write_csv_person"
+                "users": self._user_keys ,
+                "movies": self._movie_keys,
+                "persons": self._person_keys
             }
-            func = switcher.get(argument, lambda: "")
-            func(doc, collection)
+            keys = switcher.get(collection, lambda: None)
+            self._write_collection_csv(doc, collection, keys)
 
 
+    """
+    Write headers in csv files
+    """
+    def _write_csv_headers(self):
+        csv_files_headears = {"persons.csv": self._person_keys,
+                              "movies.csv": self._movie_keys,
+                              "users.csv": self._user_keys,
+                              "user_movie.csv": self._user_movie_key,
+                              "person_movie.csv": self._person_movie_key}
+        for f in csv_files_headears.keys():
+            self._write_csv_row(f, csv_files_headears[f], mode="w")
 
-    ##TODO 
+
+    """
+    Write row on csv
+    """
     def _write_csv_row(self, file_name,row, mode="a"):
         f = open("dataset/"+ file_name, mode)
-        f.write(";".join(row))
+        format_row = [('"' + r + '"') if isinstance(r, str) else str(r)  for r in row]
+        f.write(";".join(format_row) + "\n")
         f.close()
 
-    def write_csv_movie(self, doc, collection):
-        print (doc)
-        movie_row = []
-        file_name = collection + ".csv"
+    """
+    write relationship between user and movie
+    """
+    def _write_user_movie_csv(self, doc):
+        for r in doc["ratings"]:
+            row = [doc["id"], r["movie"]["id"], r["global"], r["rate"]]
+            self._write_csv_row("user_movie.csv", row)
+
+    """
+    write relationship between person and movie
+    """
+    def _write_person_movie_csv(self, doc):
         for k in doc.keys():
-            movie_row.append(doc[k])
-        self._write_csv_row(file_name, movie_row)
+            if isinstance(doc[k], list):
+                for movie in doc[k]:
+                    row = [doc["id"], movie["id"], k]
+                    self._write_csv_row("person_movie.csv", row)
 
-    def write_csv_user(self, doc, collection):
-        return
-
-    def write_csv_person(self, doc, collection):
-        return
-
-
+    """
+    Write a collection row on csv
+    """
+    def _write_collection_csv(self, doc, collection, keys):
+        row = []
+        file_name = collection + ".csv"
+        for k in keys:
+            #movie rating is more complex
+            if "rtng_" in k:
+                _,group_key,r_type = k.split("_")
+                try:
+                    for rate in doc["rating"]:
+                        if rate["group"] == group_key:
+                            if r_type == "v":
+                                field = rate["votes"]
+                            else:
+                                field = rate["rating"]
+                            break
+                except:
+                    field =""
+            else: 
+                try :
+                    if  isinstance(doc[k], list):
+                        field = ",".join(doc[k])
+                    else:
+                        field = doc[k]
+                except:
+                    field = ""
+            row.append(field)
+        self._write_csv_row(file_name, row)
+        if collection == "persons":
+            self._write_person_movie_csv(doc)
 
 
 
@@ -110,9 +177,14 @@ class IMDBStorageManager():
                         "ratings": [new_rate]
                    }
            self.write(user_info, collection="users")
+           if CSV:
+                self._write_user_movie_csv(user_info)
            return
         user = search_result[0]
         user["ratings"].append(new_rate)
         col.update(user["__id"], user)
         self._db.commit()
+        if CSV:
+            self._write_user_movie_csv(user)
+
 
