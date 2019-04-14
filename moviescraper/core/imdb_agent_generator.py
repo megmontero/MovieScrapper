ecb34fb97b5b4b979410a0ea0cd9c0f8f35d5386
WikiWikiWiki <goyo566@gmail.com>
2019-04-14 19:56:26 +0200

    PEP8 code style guidelines acomplished for imdb_agent_generator.py

--- moviescraper/core/imdb_agent_generator.py
+++ moviescraper/core/imdb_agent_generator.py
@@ -2,32 +2,52 @@
 IMDBAgentGenerator
 """
 
-# from fake_useragent import UserAgent
 import random
+
 
 class IMDBAgentGenerator():
 
     class UserAgentGenerator():
         def __init__(self):
-            self._user_agent_list = ['Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.99 Safari/537.36',
-                                     'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.99 Safari/537.36',
-                                     'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.99 Safari/537.36',
-                                     'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_1) AppleWebKit/602.2.14 (KHTML, like Gecko) Version/10.0.1 Safari/602.2.14',
-                                     'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.71 Safari/537.36',
-                                     'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.98 Safari/537.36',
-                                     'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.98 Safari/537.36',
-                                     'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.71 Safari/537.36',
-                                     'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.99 Safari/537.36',
-                                     'Mozilla/5.0 (Windows NT 10.0; WOW64; rv:50.0) Gecko/20100101 Firefox/50.0']
+            self._user_agent_list = [('Mozilla/5.0 (Windows NT 6.1; WOW64) Ap'
+                                      'pleWebKit/537.36 (KHTML, like Gecko) Ch'
+                                      'rome/54.0.2840.99 Safari/537.36'),
+                                     ('Mozilla/5.0 (Windows NT 10.0; WOW64) Ap'
+                                      'pleWebKit/537.36 (KHTML, like Gecko) Ch'
+                                      'rome/54.0.2840.99 Safari/537.36'),
+                                     ('Mozilla/5.0 (Windows NT 10.0; Win64; x6'
+                                      '4) AppleWebKit/537.36 (KHTML, like Geck'
+                                      'o) Chrome/54.0.2840.99 Safari/537.36'),
+                                     ('Mozilla/5.0 (Macintosh; Intel Mac OS X '
+                                      '10_12_1) AppleWebKit/602.2.14 (KHTML, l'
+                                      'ike Gecko) Version/10.0.1 Safari/602.2.'
+                                      '14'),
+                                     ('Mozilla/5.0 (Windows NT 10.0; WOW64) Ap'
+                                      'pleWebKit/537.36 (KHTML, like Gecko) Ch'
+                                      'rome/54.0.2840.71 Safari/537.36'),
+                                     ('Mozilla/5.0 (Macintosh; Intel Mac OS X '
+                                      '10_12_1) AppleWebKit/537.36 (KHTML, lik'
+                                      'e Gecko) Chrome/54.0.2840.98 Safari/537'
+                                      '.36'),
+                                     ('Mozilla/5.0 (Macintosh; Intel Mac OS X '
+                                      '10_11_6) AppleWebKit/537.36 (KHTML, lik'
+                                      'e Gecko) Chrome/54.0.2840.98 Safari/537'
+                                      '.36'),
+                                     ('Mozilla/5.0 (Windows NT 6.1; WOW64) App'
+                                      'leWebKit/537.36 (KHTML, like Gecko) Chr'
+                                      'ome/54.0.2840.71 Safari/537.36'),
+                                     ('Mozilla/5.0 (Windows NT 6.1; Win64; x64'
+                                      ') AppleWebKit/537.36 (KHTML, like Gecko'
+                                      ') Chrome/54.0.2840.99 Safari/537.36'),
+                                     ('Mozilla/5.0 (Windows NT 10.0; WOW64; rv'
+                                      ':50.0) Gecko/20100101 Firefox/50.0')]
             self._num_agents = len(self._user_agent_list) - 1
 
         def random(self):
             return self._user_agent_list[random.randint(0, self._num_agents)]
-                                     
+
     def __init__(self):
-        # self._generator = UserAgent(use_cache_server=False, verify_ssl=False)
         self._generator = self.UserAgentGenerator()
 
     def random_agent(self):
-        # return self.generator.random
         return self._generator.random()
